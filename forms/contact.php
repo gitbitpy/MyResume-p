ccecee56fb157440c4a06e040e78ba27ab30a771
ahmadusman <ahmadusman.5.au@gmail.com>
2024-04-27 20:50:35 +0100

    update

--- forms/contact.php
+++ forms/contact.php
@@ -67,6 +67,6 @@ if (isset($_POST['email'])) {
         'Reply-To: ' . $email . "\r\n" .
         'X-Mailer: PHP/' . phpversion();
     @mail($email_to, $email_subject, $email_message, $headers);
-  
+  return $error_message;
   }
 ?>
