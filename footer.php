62fb60713654fe67559fcb0d79328ed8ad446173
ahmadusman <ahmadusman.5.au@gmail.com>
2024-04-29 00:53:22 +0100

    update

---
+++ footer.php
@@ -0,0 +1,34 @@
+ <!-- ======= Footer ======= -->
+ <footer id="footer">
+    <div class="container">
+
+      <div class="footer-row">
+        <div class="social-links">
+          <a href="https://www.linkedin.com/in/ahmad-usman-13a897191/" class="linkedin"><i class="bx bxl-linkedin"></i></a>
+          <a href="https://github.com/gitbitpy" class="Github"><i class="bi bi-github"></i></a>
+        </div>
+        <div class="copyright">
+          &copy; Copyright <strong><span>AhmadUsman</span></strong>. All Rights Reserved
+        </div>
+      </div>
+    </div>
+  </footer><!-- End Footer -->
+
+  <div id="preloader"></div>
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+  <!-- Vendor JS Files -->
+  <script src="assets/vendor/purecounter/purecounter_vanilla.js"></script>
+  <script src="assets/vendor/aos/aos.js"></script>
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+  <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+  <script src="assets/vendor/typed.js/typed.umd.js"></script>
+  <script src="assets/vendor/waypoints/noframework.waypoints.js"></script>
+  <!-- Template Main JS File -->
+  <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
--- forms/contact.php
+++ forms/contact.php
@@ -21,24 +21,17 @@ if (array_key_exists('email', $_POST)) {
     //Create a new PHPMailer instance
     $mail = new PHPMailer();
     //Send using SMTP to localhost (faster and safer than using mail()) – requires a local mail server
-    //See other examples for how to use a remote server such as gmail
-    $mail->isSMTP();
-    $mail->Host = 'localhost';
-    $mail->Port = 25;
+
+        //Set who the message is to be sent from
+        $mail->setFrom('hi@ahmadusman.com');
 
     //Use a fixed address in your own domain as the from address
     //**DO NOT** use the submitter's address here as it will be forgery
     //and will cause your messages to fail SPF checks
-    $mail->setFrom('from@example.com', 'First Last');
     //Choose who the message should be sent to
     //You don't have to use a <select> like in this example, you can simply use a fixed address
     //the important thing is *not* to trust an email address submitted from the form directly,
     //as an attacker can substitute their own and try to use your form to send spam
-    $addresses = [
-        'sales' => 'sales@example.com',
-        'support' => 'support@example.com',
-        'accounts' => 'accounts@example.com',
-    ];
     //Validate address selection before trying to use it
 
         //Fall back to a fixed address if dept selection is invalid or missing
